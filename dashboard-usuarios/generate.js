55ef470a9cc92aba5d732d92a7adbbc25334337c
CAROLINE ROCHA RODRIGUES <caroline.rodrigues117@etec.sp.gov.br>
2025-05-30 10:34:31 -0300

    teste

---
+++ dashboard-usuarios/generate.js
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker/locale/pt_BR';
+import lodash from 'lodash';
+import fs from 'fs';
+
+const peoples = lodash.times(50, function(n){
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  return {
+    id: n+1,
+    firstname: faker.person.firstName(),
+    lastname: faker.person.lastName(),
+    avatar: faker.image.avatar(),
+    address: faker.location.streetAddress(),
+    email: faker.internet.email({firstName: firstName.toLowerCase(), lastName: lastName.toLowerCase()})
+  }
+});
+
+const data = {};
+data.peoples = peoples;
+fs.writeFile('db.json', JSON.stringify(data), (err) => { // o arquivo db será atualizado com a massa de dados fakes
+  if(err) throw err;
+  console.log('Finalizado...');
+});
